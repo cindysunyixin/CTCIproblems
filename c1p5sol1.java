0dac03f9ddb1ce1fbe8c275aa164f91b18c73beb
Yixin Sun <sun955967@gmail.com>
2016-06-19 13:22:57 -0400

    Update completed solution1/LeetCode Version Submitted

--- c1p5sol1.java
+++ c1p5sol1.java
@@ -11,7 +11,7 @@ public class c1p5sol1{
         System.out.println(OneAway(linea, lineb));
      }
 
-     public static boolean OneAway(String worda, wordb) {
+     public static boolean OneAway(String worda, String wordb) {
          if (worda.length() == wordb.length()) {
              return oneEditReplace(worda, wordb);
          } else if (worda.length() + 1 == wordb.length()) {
@@ -19,11 +19,40 @@ public class c1p5sol1{
          } else if (worda.length() - 1 == wordb.length()) {
              return oneEditInsert(wordb, worda);
          }
+         return false;
 
      }
 
      public static boolean oneEditReplace(String s1, String s2) {
-         
+         boolean foundDiff = false;
+         for (int i = 0; i < s1.length(); i++) {
+             if (s1.charAt(i) != s2.charAt(i)) {
+                 foundDiff = true;
+                 if (foundDiff) {
+                     return false;
+                 }
+             }
+
+         }
+         return true;
+     }
+
+     /* insert a character into s1 to make s1 become s2 */
+     public static boolean oneEditInsert(String s1, String s2) {
+         int index1 = 0;
+         int index2 = 0;
+         while (index2 < s2.length() && index1 < s1.length()) {
+             if (s1.charAt(index1) != s2.charAt(index2)) {
+                 if (index1 != index2) {
+                     return false;
+                 }
+                 index2++;
+             } else {
+                 index1++;
+                 index2++;
+             }
+         }
+         return true;
      }
 
 

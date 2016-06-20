e9d428b7a8843c76ed7a91ca80701fd17447ba8d
Yixin Sun <sun955967@gmail.com>
2016-06-20 15:38:17 -0400

    String compression

---
+++ c1p6.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+import java.util.HashMap;
+public class c1p6{
+
+     public static void main(String []args){
+        Scanner sc = new Scanner(System.in);
+        String line = sc.nextLine();
+        System.out.println(StringCompression(line));
+
+
+     }
